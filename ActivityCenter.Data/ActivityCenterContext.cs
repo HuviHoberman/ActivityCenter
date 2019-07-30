31fb016e172ddd064da5d5f8d8fdbab6d4d9732c
Huvi Hoberman <huvihoberman@gmail.com>
2019-07-30 18:00:22 -0400

    Initial

---
+++ ActivityCenter.Data/ActivityCenterContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ActivityCenter.Data
+{
+	public class ActivityCenterContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Activity> Activities { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<CategoryActivity> CategoriesActivities { get; set; }
+		public DbSet<GroupSize> GroupSize { get; set; }
+		private string _connectionString;
+		public ActivityCenterContext(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(_connectionString);
+		}
+	}
+}
---
+++ ActivityCenter.Data/ActivityCenterContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ActivityCenter.Data
+{
+	class ActivityCenterContextFactory : IDesignTimeDbContextFactory<ActivityCenterContext>
+	{
+		public ActivityCenterContext CreateDbContext(string[] args)
+		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), $"..{Path.DirectorySeparatorChar}ActivityCenter.Web"))
+				.AddJsonFile("appsettings.json")
+				.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true).Build();
+
+			return new ActivityCenterContext(config.GetConnectionString("ConStr"));
+		}
+	}
+}
